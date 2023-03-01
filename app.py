139192f4b03d22c0bcb1059a0c01097046e5f26e
zanis <zanis.timsans@gmail.com>
2023-03-01 13:08:34 +0200

    update libraries

--- app.py
+++ app.py
@@ -4,9 +4,9 @@
 
 # Load libraries
 import dash
-import dash_core_components as dcc
+from dash import dcc
 import dash_bootstrap_components as dbc
-import dash_html_components as html
+from dash import html
 import numpy as np
 import pandas as pd
 import plotly.graph_objects as go
