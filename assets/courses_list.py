ea5857812bacbdb3b4f30aa8d6515ecd350b3567
zanis <zanis.timsans@gmail.com>
2023-09-14 20:19:31 +0300

    Add courses

--- assets/courses_list.py
+++ assets/courses_list.py
@@ -1,5 +1,8 @@
-# 'course title': [course_id, disable]
+# 'course title': [course_id, disabled]
 _courses = {
     'Izklājlapu izmantošanas iespējas datu analīzei': [8, False],
-    'Informācijas tehnoloģijas pamatizglītībā': [79, False]
+    'Informācijas tehnoloģijas pamatizglītībā': [79, False],
+    'Ievads lietotāju pieredzē': [55, False],
+    'Stāstniecības lietpratība': [83, False],
+    'Programmēšanas valodas Python pamati': [42, False]
 }
