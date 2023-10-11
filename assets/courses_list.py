90f91ecdaf1e112aeed1ddcebbb98b31435b264a
Zanis <zanis.timsans@rtu.lv>
2023-10-11 10:56:05 +0300

    Update courses_list.py
    Add courses:
    Matemātiskās metodes datu analīzē - https://eduaim.moodle.mii.lv/course/view.php?id=49
    Datu analīze lēmumu pieņemšanai - https://eduaim.moodle.mii.lv/course/view.php?id=50
    Datu ieguve un zināšanu atklāšana - https://eduaim.moodle.mii.lv/course/view.php?id=51
    Datu drošība informāciju tehnoloģijās (4.2.) - https://eduaim.moodle.mii.lv/course/view.php?id=46
    Python pamati datu apstrādei - https://eduaim.moodle.mii.lv/course/view.php?id=47
    Digitālā kriminālistika (4.2.) - https://eduaim.moodle.mii.lv/course/view.php?id=57
    Lietotāju pieredzes psiholoģiskais pamats (3.1.) - https://eduaim.moodle.mii.lv/course/view.php?id=24
    Padziļināta informācijas pratība (1.2.) - https://eduaim.moodle.mii.lv/course/view.php?id=69
    Informācijpratības pamati (1.2.) - https://eduaim.moodle.mii.lv/course/view.php?id=14

--- assets/courses_list.py
+++ assets/courses_list.py
@@ -5,5 +5,14 @@ _courses = {
     'Ievads lietotāju pieredzē': [55, False],
     'Stāstniecības lietpratība': [83, False],
     'Programmēšanas valodas Python pamati': [42, False],
-    'Datu pratība': [5, False]
+    'Datu pratība': [5, False],
+    'Matemātiskās metodes datu analīzē': [49, False],
+    'Datu analīze lēmumu pieņemšanai': [50, False],
+    'Datu ieguve un zināšanu atklāšana': [51, False],
+    'Datu drošība informāciju tehnoloģijās': [46, False],
+    'Python pamati datu apstrādei': [47, False],
+    'Digitālā kriminālistika': [57, False],
+    'Lietotāju pieredzes psiholoģiskais pamats': [24, False],
+    'Padziļināta informācijas pratība': [69, False],
+    'Informācijpratības pamati': [14, False]
 }
